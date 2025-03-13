ca9136141ede3d9f6fb309ee180702a4a54dc6ba
Sunghwan Moon <97149338+sunghwanism@users.noreply.github.com>
2025-03-14 01:39:02 +0900

    0012-integer-to-roman_Medium_03-14-2025

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -43,4 +43,22 @@ class Solution:
                 result += str(roman_dict[order]) + str(roman_dict[(count+1)*order])
                 # print("Add", str(roman_dict[order]) + str(roman_dict[(count+1)*order]))
 
+            else:
+                print(count)
+                
         return result
+
+"""
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], 
+                    ["L", 50], ["XC", 90], ["C", 100], ["CD", 400],["D", 500],["CM", 900],["M", 1000]]
+        res = ''
+        for sym, val in reversed(symList):
+            count = num//val
+            if count:
+                res += count * sym
+                num = num % val
+        return res
+
+"""
