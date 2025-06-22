9e302296e8d8a1cad65de9feb87bc659ead8b8e5
Sunghwan Moon <97149338+sunghwanism@users.noreply.github.com>
2025-06-22 22:35:48 +0900

    0101-symmetric-tree_Easy_06-22-2025

--- LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.py
+++ LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,17 +6,43 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
+        # **recursive Solution**
+        # def symmetric(t1: TreeNode, t2: TreeNode):
+        #     if not t1 and not t2:
+        #         return True
+        #     if not t1 or not t2:
+        #         return False
+        #     if t1.val != t2.val:
+        #         return False
+        #     return symmetric(t1.left, t2.right) and symmetric(t1.right, t2.left)
 
-        def isMirror(t1: TreeNode, t2: TreeNode) -> bool:
-            if not t1 and not t2:
-                return True
-            if not t1 or not t2:
-                return False
-            if t1.val != t2.val:
-                return False
-            return isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
+        # return symmetric(root.left, root.right)
 
-        return isMirror(root.left, root.right) if root else True
+        # **Iterative Solution**
+
+        que = [(root.left, root.right)]
+
+        while que:
+            node = que[0]
+            que = que[1:]
+
+            if not node[0] and not node[1]:
+                continue
+            
+            if not node[0] or not node[1]:
+                return False
+
+            if node[0].val != node[1].val:
+                return False
+
+            que.append([node[0].left, node[1].right])
+            que.append([node[0].right, node[1].left])
+        
+        return True
+
+
+
+
 
 
                 
